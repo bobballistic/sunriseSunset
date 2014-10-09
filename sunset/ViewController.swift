fbbaa306938bd9f63fc8bf3f7b27cd216c05a847
bobballistic <bologa.bogdan@gmailcom>
2014-10-09 20:56:05 +0100

    finnished

--- sunset/ViewController.swift
+++ sunset/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sunrise: UILabel!
+    @IBOutlet var sunset: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        var url = "http://www.timeanddate.com/astronomy/uk/london"
+        var urlString = NSURL(string: url)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(urlString) {(data, response, error) in
+        
+            var urlContent = NSString(data: data, encoding: NSUTF8StringEncoding)
+            var contentArraySunrise = urlContent.componentsSeparatedByString("Sunrise Today: </span><span class=three>")
+            var newContentArraySunrise = contentArraySunrise[1].componentsSeparatedByString("</span>")
+            println(newContentArraySunrise[0])
+            dispatch_async(dispatch_get_main_queue()){
+            self.sunrise.text = newContentArraySunrise[0] as? String
+            }
+            
+            var contentArraySunset = urlContent.componentsSeparatedByString("Sunset Today: </span><span class=three>")
+            var newContentArraySunset = contentArraySunset[1].componentsSeparatedByString("</span>")
+            println(newContentArraySunset[0])
+            dispatch_async(dispatch_get_main_queue()){
+            self.sunset.text = newContentArraySunset[0] as? String
+            }
+            }
+        task.resume()
+    
     }
 
     override func didReceiveMemoryWarning() {
