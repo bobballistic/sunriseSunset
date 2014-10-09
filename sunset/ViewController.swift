6fc3be34ab82d1b24b3429475128bd6f9abc1f26
bobballistic <bologa.bogdan@gmailcom>
2014-10-09 19:55:15 +0100

    Initial Commit

---
+++ sunset/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sunset
+//
+//  Created by Bob on 09/10/2014.
+//  Copyright (c) 2014 BallisticSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
